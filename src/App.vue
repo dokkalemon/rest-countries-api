39bedf1e056b8b90c1e4519006c5290425bb57bd
dokkalemon <dottori.roberto@gmail.com>
2022-01-10 18:47:41 +0100

    add Header components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <h1>prova</h1>
+  <div id="app">
+    <Header />
+
+
+  </div>
 </template>
 
 <script>
+import Header from '@/components/Header.vue'
 
 export default {
   name: 'App',
   components: {
+    Header
     
   }
 }
---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+    <header class="full-width">
+        <div class="header container px-30">
+            <h2>Where in the world?</h2>
+            <button><i class="fas fa-sun"></i> Light Mode</button>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped lang="scss">
+//import var.scss
+@import '../styles/var.scss';
+
+    header {
+        background-color: $dark-element;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.413);
+        .header {
+            height: 80px;
+            display: flex;
+            align-items: center;
+            h2 {
+                color: $dark-text;
+                font-weight: 600;
+                flex-grow: 1;
+            }
+
+            button {
+                background-color: transparent;
+                border: none;
+                color: $dark-text;
+                font-size: 16px;
+                cursor: pointer;
+                padding: 10px;
+                transition: all 0.5s ease;
+                border-radius: 20px;
+                &:hover {
+                    background-color: rgba(255, 255, 255, 0.036);
+                }
+            }
+        }
+    }
+</style>
